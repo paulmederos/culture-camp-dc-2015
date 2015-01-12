00e9bd0d63a8d1daad508a49c1888ec56b882870
Paul Mederos <pvm@enchant.co>
2015-01-11 16:42:34 -0800

    Removing unecessary scripts.

--- functions.php
+++ functions.php
@@ -99,17 +99,10 @@ add_action( 'widgets_init', 'culture2015_widgets_init' );
 function culture2015_scripts() {
 	wp_enqueue_style( 'culture2015-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'culture2015-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'culture2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'culture2015-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'culture2015_scripts' );
 
